a2d3d4caca762bf224d8821a2f1106af53c77ea5
66Anony123 <478gnujnoej@gmail.com>
2024-04-11 22:29:35 +0545

    Frontend Initialized

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const routes = require("./routes/index");
 // app.use({extended:cors});
 app.use("/", routes);
 
+app.all("*", (req, res) => {
+  res.send("ERRor 404, Page Not Found");
+});
+
 app.listen(5000, () => {
   console.log("Port listening in 5000");
 });
